68177961aac6409f2262cbaec6fe22ae7242dfa2
tutututurbo <162029884+tutututurbo@users.noreply.github.com>
2024-11-21 11:50:43 +0100

    Fix routes and static files

--- app.py
+++ app.py
@@ -23,10 +23,11 @@ def clear_frames_folder():
     else:
         os.makedirs(frames_folder)
 
+
+
 @app.route('/')
 def home():
-    # Renderizza la tua pagina HTML
-     return "Applicazione Flask è attiva e funzionante!"
+    return render_template('index.html')  # Serve la pagina HTML
 
 @app.route('/upload-video', methods=['POST'])
 def upload_video():
