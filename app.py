45242f32f2545782eea97e0636089d49c3a3b49e
tutututurbo <162029884+tutututurbo@users.noreply.github.com>
2024-11-21 12:10:37 +0100

    fritgahkuw<blmtòvr

--- app.py
+++ app.py
@@ -64,4 +64,4 @@ def upload_video():
     return jsonify({'message': frame_count})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5001))
